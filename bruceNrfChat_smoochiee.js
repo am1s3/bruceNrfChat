ae3cf80e3121eb3ae1e9a26ca86129b8db2a9bd7
am1se <shushko.art@gmail.com>
2026-07-04 15:32:33 +0300

    Smoochiee V2

--- bruceNrfChat_smoochiee.js
+++ bruceNrfChat_smoochiee.js
@@ -1,6 +1,8 @@
 var role = "initiator";
 var text = "";
 var receivedText = "";
+var sharedSecret = 0;
+var dhComplete = false;
 var keyboardLayout = [
     ["1","2","3","4","5","6","7","8","9","0"],
     ["Q","W","E","R","T","Y","U","I","O","P"],
@@ -13,29 +15,46 @@ var cols = 10;
 var selectedRow = 0;
 var selectedCol = 0;
 var mode = "keyboard";
-
 var nrfCE = 9;
 var nrfCSN = 8;
 var nrfSCK = 14;
 var nrfMOSI = 13;
 var nrfMISO = 12;
-
 var btnUP = 5;
 var btnDOWN = 39;
 var btnLEFT = 40;
 var btnRIGHT = 41;
 var btnSELECT = 16;
 var btnBACK = 42;
-
+var p = 2147483647;
+var g = 5;
+var privateKey = Math.floor(Math.random() * (p - 2)) + 2;
+var publicKey = modPow(g, privateKey, p);
 if (gpio.read(btnUP) == 0) { role = "responder"; }
-
 nrf.init(nrfSCK, nrfMISO, nrfMOSI, nrfCSN, nrfCE);
 nrf.setChannel(100);
 nrf.setDataRate("250kbps");
 nrf.setPower("max");
-
 display.clear();
-
+function modPow(base, exp, mod) {
+    var result = 1;
+    base = base % mod;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        exp = exp >> 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
+function encryptDecrypt(msg, key) {
+    var out = "";
+    for (var i = 0; i < msg.length; i++) {
+        out += String.fromCharCode(msg.charCodeAt(i) ^ key);
+    }
+    return out;
+}
 function drawKeyboard() {
     display.clear();
     display.println("Chat (" + role + ")");
@@ -57,7 +76,6 @@ function drawKeyboard() {
     }
     display.println("Keys: UP/DOWN/LEFT/RIGHT | SELECT=choose | BACK=exit");
 }
-
 function drawChat() {
     display.clear();
     display.println("Chat (" + role + ")");
@@ -69,9 +87,36 @@ function drawChat() {
     display.println("----------------");
     display.println("Press SELECT to send | BACK to exit");
 }
-
+function dhExchange() {
+    display.println("DH: exchanging keys...");
+    if (role == "initiator") {
+        nrf.send(publicKey.toString());
+        var received = nrf.receive();
+        while (received == null || received == "") {
+            delay(100);
+            received = nrf.receive();
+        }
+        var otherPublic = parseInt(received);
+        sharedSecret = modPow(otherPublic, privateKey, p);
+    } else {
+        var received = nrf.receive();
+        while (received == null || received == "") {
+            delay(100);
+            received = nrf.receive();
+        }
+        var otherPublic = parseInt(received);
+        nrf.send(publicKey.toString());
+        sharedSecret = modPow(otherPublic, privateKey, p);
+    }
+    dhComplete = true;
+    display.println("DH: secret established.");
+    display.println("Secret: " + sharedSecret);
+    delay(1000);
+    display.clear();
+}
+display.println("Starting DH key exchange...");
+dhExchange();
 drawKeyboard();
-
 while (true) {
     if (mode == "keyboard") {
         if (gpio.read(btnUP) == 0) {
@@ -106,7 +151,8 @@ while (true) {
                 text = "";
             } else if (key == "SEND") {
                 if (text.length > 0) {
-                    nrf.send(text);
+                    var encrypted = encryptDecrypt(text, sharedSecret);
+                    nrf.send(encrypted);
                     receivedText = "";
                     mode = "chat";
                     drawChat();
@@ -119,7 +165,8 @@ while (true) {
         }
         if (gpio.read(btnBACK) == 0) {
             if (text.length > 0) {
-                nrf.send(text);
+                var encrypted = encryptDecrypt(text, sharedSecret);
+                nrf.send(encrypted);
                 receivedText = "";
                 mode = "chat";
                 drawChat();
@@ -128,7 +175,8 @@ while (true) {
         }
         var incoming = nrf.receive();
         if (incoming != null && incoming != "") {
-            receivedText = incoming;
+            var decrypted = encryptDecrypt(incoming, sharedSecret);
+            receivedText = decrypted;
             drawKeyboard();
         }
         delay(50);
@@ -147,7 +195,8 @@ while (true) {
         }
         var incoming = nrf.receive();
         if (incoming != null && incoming != "") {
-            receivedText = incoming;
+            var decrypted = encryptDecrypt(incoming, sharedSecret);
+            receivedText = decrypted;
             drawChat();
         }
         delay(50);
